9f9e1228148f71db26597b258c8d0c5073ce208b
Jeonghun <53068105+sunrinjh@users.noreply.github.com>
2025-06-01 18:26:29 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -36,7 +36,7 @@ const CONFIG = {
 
   // plugin configuration (optional)
   googleAnalytics: {
-    enable: true,
+    enable: false,
     config: {
       measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
     },
